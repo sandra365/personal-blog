e782da8b8366585e387e578c92cfcd941a7708a8
sandra365 <postnsanna@gmail.com>
2024-01-10 17:04:17 +0100

    first commit

---
+++ src/components/BlogPost.js
@@ -0,0 +1,12 @@
+const BlogPost = () => {
+    return(
+        <div>
+            <h2 className="post-title">Post</h2>
+            <article className="post-body">Post body</article>
+            <button>Read more</button>
+        </div>
+    );
+    
+}
+
+export default BlogPost;
